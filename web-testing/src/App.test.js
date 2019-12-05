633d05a371da9b8ba681ff346ed618de83503154
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-12-05 12:17:16 +0000

    Project set up

---
+++ web-testing/src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App';
+
+
+afterEach(rtl.cleanup)
+
+it('renders without crashing', () => {
+const wrapper = rtl.render(<App />)
+wrapper.debug()
+
+});
